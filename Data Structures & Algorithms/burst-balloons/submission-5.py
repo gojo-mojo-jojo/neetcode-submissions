bf419b6c3dafdca0e4184264fe1b300e9ea77daf
Pratik <66382460+gojo-mojo-jojo@users.noreply.github.com>
2026-07-30 14:55:10 +0530

    Add: burst-balloons - submission-5

---
+++ Data Structures & Algorithms/burst-balloons/submission-5.py
@@ -0,0 +1,29 @@
+from functools import cache
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        #Interval DP (use interval state (l,r))
+
+
+        nums = [1] + nums + [1]
+        dp = {}
+        #@cache
+        def dfs(l, r):
+
+            if l + 1 >= r:
+                return 0
+            
+            if (l, r) in dp:
+                return dp[(l,r)]
+
+            best = 0
+            for i in range(l+1, r):
+
+                coins = nums[l] * nums[i] * nums[r] #its the last one so 1*nums[i]*1 = nums[i]
+                coins += dfs(l,i) + dfs(i, r)
+                best = max(best, coins)
+            
+            dp[(l,r)] = best
+            return dp[(l,r)] 
+        
+        return dfs(0, len(nums)-1)
+        
